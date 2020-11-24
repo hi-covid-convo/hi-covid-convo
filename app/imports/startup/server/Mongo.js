6685dc80cfbf907bdb5c63467986512f30549497
Sydney Dempsey <dempseys@hawaii.edu>
2020-11-24 11:04:28 -1000

    changed default data for feedback

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -26,7 +26,7 @@ if (Stuffs.collection.find().count() === 0) {
   if (Reviews.collection.find().count() === 0) {
     if (Meteor.settings.defaultData) {
       console.log('Creating default feedback data.');
-      Meteor.settings.defaultData.map(data => addReview(data));
+      Meteor.settings.defaultFeedback.map(data => addReview(data));
     }
 }
 
