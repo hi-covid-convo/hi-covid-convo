bc1686cdb312bba0a3adfef91b4867a971117afc
Glen Larita <glarita@hawaii.edu>
2020-11-23 15:39:10 -1000

    Updated chatbot design, made design changes to app

--- app/imports/ui/pages/About.jsx
+++ app/imports/ui/pages/About.jsx
@@ -4,20 +4,24 @@ import { Container, Header } from 'semantic-ui-react';
 export default class About extends React.Component {
   render() {
     return (
-        <Container text>
-          <Header as='h1' textAlign='center'>About Covid Convo</Header>
-          <p>Hawaii Covid Convo was made to improve the customer experience while providing easy navigation and providing
-            useful and helpful resources for Hawaii.
-          </p>
-          <p>We wanted to use a chat-bot interface to provide a friendlier and more personalized experience for the
-            users. We know that many residents and tourists have a difficult time finding the appropriate resources
-            when it comes to Covid-19 in the state of Hawaii. Our chat-bot Cece will provide an easier way to access
-            the resources that people are looking for.
-          </p>
-          <p>Whether you are trying to find a location to get tested for COVID-19, or you want to just stay updated with
-            the latest news on the virus, Cece will be able to assist anyone and their COVID-19 requests!
-          </p>
-        </Container>
+        <div className='text'>
+          <Container text>
+            <Header as='h1' textAlign='center'>About Covid Convo</Header>
+            <p>Hawaii Covid Convo was made to improve the customer experience while providing easy navigation and
+              providing
+              useful and helpful resources for Hawaii.
+            </p>
+            <p>We wanted to use a chat-bot interface to provide a friendlier and more personalized experience for the
+              users. We know that many residents and tourists have a difficult time finding the appropriate resources
+              when it comes to Covid-19 in the state of Hawaii. Our chat-bot Cece will provide an easier way to access
+              the resources that people are looking for.
+            </p>
+            <p>Whether you are trying to find a location to get tested for COVID-19, or you want to just stay updated
+              with
+              the latest news on the virus, Cece will be able to assist anyone and their COVID-19 requests!
+            </p>
+          </Container>
+        </div>
     );
   }
 }
--- app/imports/ui/pages/Cece.jsx
+++ app/imports/ui/pages/Cece.jsx
@@ -10,22 +10,22 @@ export default class Cece extends React.Component {
       backgroundColor: "#2B313E",
     }
     return (
-        <div class="chatbot">
+        <div className="chatbot">
           <Grid container centered stackable columns={2}>
 
             <Grid.Column textAlign='center'>
-              <Header as='h1' inverted>Meet Cece!</Header>
-              <Header as='h3' inverted>
+              <Header as='h1' inverted color='grey'>Meet Cece!</Header>
+              <Header as='h3' inverted color='grey'>
                 Meet the Hawaii Covid-19 chat-bot, Cece! If you have any questions that you have wanted to
                 ask about Covid-19, you can just ask in the message box to the right, and Cece will be able to help you!
               </Header>
-              <Header as='h3' inverted>
+              <Header as='h3' inverted color='grey'>
                 Whether you are unsure of what the current Covid-19 rules and regulations in Hawaii are, or if you want
                 to
                 find Covid-19 testing locations on the island, Cece will be able to help you and provide the appropriate
                 sources for you so you don&apos;t have to do any research or searching!
               </Header>
-              <Header as='h4' inverted>
+              <Header as='h4' inverted color='grey'>
                 Let us know what you think about Cece, and please fill out this survey to let us know how helpful Cece
                 was!
               </Header>
