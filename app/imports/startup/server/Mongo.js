871da23e3cf22f4a9289bd0571253a68f217963e
duhkneelow <dnilo@my.hpu.edu>
2020-12-13 11:25:14 -1000

    Had to make sure bits of code was in sync with main branch

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -11,13 +11,6 @@ function addReview(data) {
 
 /** Initialize the collection if empty. */
 if (Reviews.collection.find().count() === 0) {
-  if (Meteor.settings.defaultReviews) {
-    console.log('Creating default feedback data.');
-    Meteor.settings.defaultReviews.map(data => addReview(data));
-  }
-}
-
-if (Reviews.collection.find().count() === 0) {
   if (Meteor.settings.defaultFeedback) {
     console.log('Creating default feedback data.');
     Meteor.settings.defaultFeedback.map(data => addReview(data));
